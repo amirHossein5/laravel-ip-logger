7f8ade77c226cfdee9fba28b77ba75a71943531f
amirHossein5 <adada5116@gmail.com>
2022-02-07 20:31:54 +0330

    config file

--- config/ipLogger.php
+++ config/ipLogger.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    // see supproted sites 
+    // see supproted apis https://github.com/amirHossein5/laravel-ip-logger/tree/main#manually-getting-details
     'get_details_from' => 'ip_api',
 
     'vpn_api_key' => null,
